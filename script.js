19ba06c7441585dbf4bfdadf103e58bd68773b8a
dogbonezone <alexrhern@gmail.com>
2022-01-08 22:20:22 -0800

    slight update to check game logic

--- script.js
+++ script.js
@@ -5,43 +5,63 @@ function GameBoard() {
         for (let i of gameBoard) {
             let box = document.createElement('div')
             box.id = i
+            box.dataset.winValue = 0
             box.classList.add('box')
 
             // Check clicked box and have CPU select box based on difficulty level
             box.addEventListener("click", () => {
+                let playerLabel = 'player'
+                const difficulty = document.querySelector('#CPULevel').value
                 if (box.textContent == '') {
                     box.textContent = 'X'
-                    checkGameEnd(box.id)
-                    CPU()
+                    box.dataset.winValue = 1
+                    checkGameEnd(box, playerLabel)
+                    CPU(difficulty)
                 }
             })
 
             document.querySelector('.gameBoard').append(box)
         }
     }
-}
 
-function CPU() {
-    easy: {
-        // Random Unchecked Box Strategy
-        if (document.querySelector("#CPULevel").value == 'Easy') {
-            let availableBoxes = []
-            const boxes = [...document.querySelectorAll(".box")]
-            for (let box of boxes) {
-                if (box.textContent == '') {availableBoxes.push(box.id)}
+    resetGame: {
+        const resetButton = document.querySelector("#reset")
+        resetButton.addEventListener("click", () => {
+            for (let box of [...document.querySelectorAll(".box")]) {
+                box.textContent = ''
+                box.dataset.winValue = 0
             }
-            let index = Math.floor(availableBoxes.length * Math.random())
-            let boxID = availableBoxes[index]
-            document.querySelector(`#${boxID}`).textContent = 'O'
-            checkGameEnd(boxID)
-        }
+        })
     }
 }
 
-function checkGameEnd(boxID) {
-    const numID = Number([...boxID].pop())
-    console.log(numID)
-    console.log(boxID)
+function CPU(difficulty) {
+    // Random Unchecked Box Strategy
+    if (difficulty == 'Easy') {
+        let playerLabel = 'cpu'
+        let availableBoxes = []
+        const boxes = [...document.querySelectorAll(".box")]
+        for (let box of boxes) {
+            if (box.textContent == '') {availableBoxes.push(box)}
+        }
+        let index = Math.floor(availableBoxes.length * Math.random())
+        let box = availableBoxes[index]
+        box.textContent = 'O'
+        box.dataset.winValue = -1
+        checkGameEnd(box, playerLabel)
+    }
+}
+
+// Need to figure out how to work Check Game Logic (has dataset label for +1 for Player and -1 for CPU)
+function checkGameEnd(box, playerLabel) {
+    const board = document.querySelector('.gameBoard')
+    console.log(box)
+    if (playerLabel === 'player') {
+
+    }
+    else {
+
+    }
 }
 
 GameBoard()
